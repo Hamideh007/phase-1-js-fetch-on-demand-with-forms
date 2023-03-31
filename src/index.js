a0786f844eeda7ca6ad75278f04e132902380fdd
hamideh rahmani <rahmani.mahtab@gmail.com>
2023-03-31 01:11:48 -0400

    complete fetch on demand

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 const init = () => {
+    const inputForm = document.querySelector("form");
+    const movieTitle = document.querySelector("#movieDetails h4");
+    const movieSummary = document.querySelector("#movieDetails p");
   
 }
 
